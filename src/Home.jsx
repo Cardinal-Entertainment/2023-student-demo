47e890a94fc51042c115853de83bb29a658f12d2
power-pole03 <powerpoleyang@gmail.com>
2023-09-20 00:52:18 +0900

    broken files

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,46 +5,51 @@ import { Link } from "wouter";
 import Leaderboard from './components/Leaderboard';
 
 function Home() {
-    const handleResetClick = () => {
-        // Save the username in local storage
-        localStorage.clear();
-    };
-
     // Retrieve leaderboard data from local storage
     const storedData = localStorage.getItem('Leaderboard');
     const leaderboardData = storedData ? JSON.parse(storedData) : [];
 
     return (
         <Container fluid className="Home-background">
-            <header className="site-header">
-              <video className="video-size" autoPlay muted>
-                <source
+            <video className="logo-video" autoPlay muted>
+                 <source
                   src="logo_animation.mp4"
                   type="video/mp4"
                   />
-              </video>
-              <audio controls autoPlay loop>
-                <source src="./sounds/homeBackground.mp3" type="audio/mpeg" />
-              </audio>
-            </header>
-            <h1 className="Home-title">
-                Sort Game
-            </h1>
-            
-            {/* Bottom components aligned */}
-            <div className="bottom-container">
-                <h3 className="Home-summary">Sort the cards into the boxes before time runs out! If you drag and drop a card in corresponding crate, you earn 1 score. Otherwise, you lose 1 score.</h3>
-                <div className="buttons">
-                    <Link href="/Game">
-                        <Button size="lg">Start game</Button>
-                    </Link>
-                    <Button size="md" onClick={handleResetClick} className="Reset-button" >Reset Leaderboard</Button>
-                </div>
-                <div className="Leaderboard">
-                    <Leaderboard data={leaderboardData} />
-                </div>
-            </div>
+            </video>
+            <h1 className="gameTitle">CLICK A MINI-GAME TO PLAY</h1>
         </Container>
+
+        // <Container fluid className="Home-background">
+        //     <header className="site-header">
+        //       <video className="video-size" autoPlay muted>
+        //         <source
+        //           src="logo_animation.mp4"
+        //           type="video/mp4"
+        //           />
+        //       </video>
+        //       <audio controls autoPlay loop>
+        //         <source src="./sounds/homeBackground.mp3" type="audio/mpeg" />
+        //       </audio>
+        //     </header>
+        //     <h1 className="Home-title">
+        //         Sort Game
+        //     </h1>
+            
+        //     {/* Bottom components aligned */}
+        //     <div className="bottom-container">
+        //         <h3 className="Home-summary">Sort the cards into the boxes before time runs out! If you drag and drop a card in corresponding crate, you earn 1 score. Otherwise, you lose 1 score.</h3>
+        //         <div className="buttons">
+        //             <Link href="/Game">
+        //                 <Button size="lg">Start game</Button>
+        //             </Link>
+        //             <Button size="md" onClick={handleResetClick} className="Reset-button" >Reset Leaderboard</Button>
+        //         </div>
+        //         <div className="Leaderboard">
+        //             <Leaderboard data={leaderboardData} />
+        //         </div>
+        //     </div>
+        // </Container>
     );
 }
 
